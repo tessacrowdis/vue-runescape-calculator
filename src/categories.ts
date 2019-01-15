14872ad7b7ee5983d9f38f4c973bd495dca9d562
Robert Crowdis <RobertCrowdis@gmail.com>
2019-01-15 12:57:58 -0500

    feat(skills): add new skills, add assets, fix rendering of local assets (#1)

--- src/categories.ts
+++ src/categories.ts
@@ -1,68 +1,153 @@
 export default [
-	{
-		name: "Fishing",
-		src: "https://oldschool.tools/images/skills/fishing.png",
-		resources: [
-			{
-				name: "Raw shrimps",
-				src: "https://general-543f.kxcdn.com/items/raw_shrimps.png",
-				level: 1,
-				experience: 10
-			},
-			{
-				name: "Raw sardine",
-				src: "https://general-543f.kxcdn.com/items/raw_sardine.png",
-				level: 5,
-				experience: 20
-			},
-			{
-				name: "Raw herring",
-				src: "https://general-543f.kxcdn.com/items/raw_herring.png",
-				level: 10,
-				experience: 30
-			},
-			{
-				name: "Raw anchovies",
-				src: "https://general-543f.kxcdn.com/items/raw_anchovies.png",
-				level: 15,
-				experience: 40
-			}
-		]
-	},
-	{
-		name: "Mining",
-		src: "https://oldschool.tools/images/skills/mining.png",
-		resources: [
-			{
-				name: "Iron",
-				src: "https://general-543f.kxcdn.com/items/iron_ore.png",
-				level: 15,
-				experience: 35
-			}
-		]
-	},
-	{
-		name: "Firemaking",
-		src: "https://oldschool.tools/images/skills/firemaking.png",
-		resources: [
-			{
-				name: "Logs",
-				src: "https://general-543f.kxcdn.com/items/logs.png",
-				level: 1,
-				experience: 40
-			},
-			{
-				name: "Oak Logs",
-				src: "https://general-543f.kxcdn.com/items/oak_logs.png",
-				level: 15,
-				experience: 60
-			},
-			{
-				name: "Willow logs",
-				src: "https://general-543f.kxcdn.com/items/willow_logs.png",
-				level: 30,
-				experience: 90
-			}
-		]
-	}
+    {
+        name: "Agility",
+        src: "skill_icons/agility.png",
+        resources: []
+    },
+    {
+        name: "Construction",
+        src: "skill_icons/construction.png",
+        resources: []
+    },
+    {
+        name: "Cooking",
+        src: "skill_icons/cooking.png",
+        resources: []
+    },
+    {
+        name: "Crafting",
+        src: "skill_icons/crafting.png",
+        resources: []
+    },
+    {
+        name: "Farming",
+        src: "skill_icons/farming.png",
+        resources: []
+    },
+    {
+        name: "Firemaking",
+        src: "skill_icons/firemaking.png",
+        resources: [
+            {
+                name: "Logs",
+                src: "https://general-543f.kxcdn.com/items/logs.png",
+                level: 1,
+                experience: 40,
+                members: false
+            },
+            {
+                name: "Oak Logs",
+                src: "https://general-543f.kxcdn.com/items/oak_logs.png",
+                level: 15,
+                experience: 60,
+                members: false
+            },
+            {
+                name: "Willow logs",
+                src: "https://general-543f.kxcdn.com/items/willow_logs.png",
+                level: 30,
+                experience: 90,
+                members: false
+            },
+            {
+                name: "Teak logs",
+                src: "https://general-543f.kxcdn.com/items/teak_logs.png",
+                level: 35,
+                experience: 105,
+                members: true
+            },
+            {
+                name: "Arctic pine logs",
+                src: "https://general-543f.kxcdn.com/items/arctic_pine_logs.png",
+                level: 42,
+                experience: 125,
+                members: true
+            },
+            {
+                name: "Maple logs",
+                src: "https://general-543f.kxcdn.com/items/maple_logs.png",
+                level: 45,
+                experience: 135,
+                members: false
+            },
+
+        ]
+    },
+    {
+        name: "Fishing",
+        src: "skill_icons/fishing.png",
+        resources: [
+            {
+                name: "Raw shrimps",
+                src: "https://general-543f.kxcdn.com/items/raw_shrimps.png",
+                level: 1,
+                experience: 10,
+                members: false
+            },
+            {
+                name: "Raw sardine",
+                src: "https://general-543f.kxcdn.com/items/raw_sardine.png",
+                level: 5,
+                experience: 20,
+                members: false
+            },
+            {
+                name: "Raw herring",
+                src: "https://general-543f.kxcdn.com/items/raw_herring.png",
+                level: 10,
+                experience: 30,
+                members: false
+            },
+            {
+                name: "Raw anchovies",
+                src: "https://general-543f.kxcdn.com/items/raw_anchovies.png",
+                level: 15,
+                experience: 40,
+                members: false
+            }
+        ]
+    },
+    {
+        name: "Fletching",
+        src: "skill_icons/fletching.png",
+        resources: []
+    },
+    {
+        name: "Herblore",
+        src: "skill_icons/herblore.png",
+        resources: []
+    },
+    {
+        name: "Mining",
+        src: "skill_icons/mining.png",
+        resources: [
+            {
+                name: "Iron",
+                src: "https://general-543f.kxcdn.com/items/iron_ore.png",
+                level: 15,
+                experience: 35,
+                members: true
+            }
+        ]
+    },
+    {
+        name: "Runecrafting",
+        src: "skill_icons/runecrafting.png",
+        resources: []
+    },
+    {
+        name: "Smiting",
+        src: "skill_icons/smithing.png",
+        resources: []
+    },
+    {
+        name: "Hunter",
+        src: "skill_icons/hunter.png",
+        resources: []
+    },
+    {
+        name: "Woodcutting",
+        src: "skill_icons/woodcutting.png",
+        resources: []
+    },
 ]
