8c8c7124aaf813ae3f62161cdd9b6c5b2aa9b0bc
Robert Crowdis <RobertCrowdis@gmail.com>
2019-01-18 16:10:04 -0500

    feat(background): modify gradient fade, style, and background height

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,13 @@
 </template>
 
 <style lang="scss">
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  height: 100vh;
-
 }
 #nav {
   padding: 30px;
